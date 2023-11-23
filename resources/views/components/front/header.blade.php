5fa89f3c1d561a5b6a312704ab4041434f912061
code353535 <125445010+code353535@users.noreply.github.com>
2023-11-23 10:28:04 +0300

    d

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ Open: false }" class="max-md:sticky max-md:top-0 z-50 bg-white" x-cloak="false">
     <!-- header -->
 <div class="flex flex-col lg:px-[170px] px-6 max-md:px-0">
-    <div class="flex flex-row justify-between md:py-4 text-black text-xs py-2 max-md:pl-4 border-gray-200 border-b-2">
+    <div class="flex flex-row justify-between md:py-4 text-black text-xs py-2 max-md:px-4 border-gray-200 border-b-2">
         <!-- enüst sol -->
         <div class="text-xs flex">{{ $date = Carbon\Carbon::now()->translatedFormat('d-m-Y l') }} - Gündemi ve Blogları Takip Et.</div>
         <!-- enüst sağ -->
