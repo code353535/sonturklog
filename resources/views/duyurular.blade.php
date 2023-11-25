ee64b4a91c94687d0942a3141ef0d1ec5dcdb227
code353535 <125445010+code353535@users.noreply.github.com>
2023-11-25 21:27:48 +0300

    f

--- resources/views/duyurular.blade.php
+++ resources/views/duyurular.blade.php
@@ -12,14 +12,14 @@
 
             <div class="flex flex-col w-full mb-4 border">
 
-            <div class="flex justify-between">
+            <div class="flex justify-between pt-2">
                 <div class="text-xs flex items-center px-2"><img src="{{ URL::asset('storage/logo/' .$fee->site?->logo) }}" class="h-6 w-6 mr-2" /> <a href="{{ $fee->site?->url}}" target="_blank" class="hover:underline">{{ $fee->site?->ad }}</a>
                 </div>
                 <div class="flex text-xs items-center">
                     <a href="{{route('front.detaylar',['id' => $fee->id, 'slug' =>  Str::slug($fee->baslik)])}}" target="_blank" class="hover:underline pr-2">detaylar</a>
                     </div>
             </div>
-            <div class="oswald flex grow px-1 text-lg font-bold pb-3 py-2 px-2"><a href="{{ $fee->url }}" id="{{$fee->id}}" class="link hover:underline" target="_blank">{{ $fee->baslik }}</a></div>
+            <div class="oswald flex grow px-1 text-lg font-bold pb-3 pt-2 px-2"><a href="{{ $fee->url }}" id="{{$fee->id}}" class="link hover:underline" target="_blank">{{ $fee->baslik }}</a></div>
             <div class="flex justify-between px-2 pb-3 text-gray700">
                 <div class="text-xs"><a href="" class="hover:underline">{{ $fee->category->ad  }}</a> </div>
                 <div class="flex text-xs items-center">{{ \Carbon\Carbon::parse($fee->pubdate)->diffForHumans() }}
