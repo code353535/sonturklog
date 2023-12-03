ccf6f8aad2569e6ef78cdd0924c07d57f0db9adf
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-03 22:03:51 +0300

    B

--- resources/views/components/front/mobilmenu.blade.php
+++ resources/views/components/front/mobilmenu.blade.php
@@ -17,8 +17,14 @@
 <div class="px-4 flex items-center"><i class="fa-solid fa-chevron-down"></i></div>
 </div>
 </a>
-
-
+<hr class="h-px px-6 my-2 border-1 border-solid border-gray-200">
+<a href="{{ route('front.habersite') }}" class="block px-4 py-2 hover:text-white hover:bg-black">
+  <div class="flex justify-between">
+<div> HABER SİTELERİ
+</div>
+<div class="px-4 flex items-center"><i class="fa-solid fa-chevron-down"></i></div>
 </div>
 </a>
+
+</div>
 </div>
