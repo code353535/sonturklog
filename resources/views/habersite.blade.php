2737d9bb1ddceb8a82030bd4e841ea0927b206a0
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-04 19:21:57 +0300

    b

--- resources/views/habersite.blade.php
+++ resources/views/habersite.blade.php
@@ -4,18 +4,18 @@
     </div>
     <hr class="lg:mx-[170px] my-6 border border-gray-200">
 
-    <div class="lg:mx-[170px] max-md:mx-4 flex flex-col xl:flex-row gap-4 mt-6">
-        <div class="w-full gap-4">
+    <div class="lg:mx-[170px] max-md:mx-4 flex flex-col xl:flex-row mt-6">
+        <div class="w-full">
             @if(count($habersite) > 0)
 
             @foreach($habersite->chunk(5) as $chunk)
-            <div class="flex flex-row max-xl:flex-wrap gap-4">
+            <div class="flex flex-row max-xl:flex-wrap md:gap-8 md:mb-4">
 
             @foreach ($chunk as $fee)
 
-            <div class="flex flex-col w-full mb-4 border bg-gray-100 md:shadow-sm md:shadow-slate-500/50 px-4 py-4">
+            <div class="flex flex-col w-full mb-4 border md:bg-gray-100 md:shadow-sm md:shadow-slate-500/50 px-4 py-4">
             <div class="flex flex-col mb-2 overflow-hidden xl:max-h-[150px] max-h-[150px] max-md:hidden">
-                <a href="{{ $fee->url }}" class="" target="_blank"> <img src="{{ URL::asset('storage/logo/' .$fee->logo) }}" class="xl:h-[150px] h-[150px] min-w-full hover:scale-110 transition duration-500 object-cover" alt=""/></a>
+                <a href="{{ $fee->url }}" class="float-left" target="_blank"> <img src="{{ URL::asset('storage/logo/' .$fee->logo) }}" class="xl:h-[150px] h-[150px] min-w-full hover:scale-110 transition duration-500 object-cover" alt=""/></a>
             </div>
 
             <div class="oswald flex grow px-1 text-2xl md:text-lg font-bold pb-3 px-2"><a href="{{ $fee->url }}" class="hover:underline" target="_blank">{{ $fee->ad }}</a></div>
