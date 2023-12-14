2f4875cc862c82fa33c02ceaa3eea4a10d9d7973
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-14 19:17:41 +0300

    b

--- app/Console/Commands/Botara.php
+++ app/Console/Commands/Botara.php
@@ -109,7 +109,7 @@ class Botara extends Command
 
                         if($feed){
                             foreach ($feed->channel->item as $article) {
-
+                                if($article->title && $article->link){
                                     $desc = $article->description;
                                     $des = strip_tags($desc);
                                     $baslik = html_entity_decode($article->title);
@@ -274,7 +274,7 @@ class Botara extends Command
                             'updated_at' => Carbon::now()
                         ]);
                     }
-
+                }
                     }
 
                 }
