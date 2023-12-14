9439694a8591994828e9f5d3fa4e654e521c38dc
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-14 19:40:25 +0300

    b

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,17 +11,8 @@ class AdminController extends Controller
 {
     public function index(){
 
-        $site = Site::where('sahip','aktif')
-        ->where('durum', 'bekliyor')
-        ->count();
 
-        $topsite = Site::all()
-        ->count();
-
-        $feedtop = Feed::all()->count();
-        $bottop = Bot::all()->count();
-
-        return view('admin.index',['site' => $site, 'topsite' => $topsite, 'feedtop' => $feedtop, 'bottop' => $bottop]);
+        return view('admin.index');
 
         }
 }
