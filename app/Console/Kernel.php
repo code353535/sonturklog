9600c84e3763f4411493b1df2ee3d07d2bd33bd5
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-16 17:48:51 +0300

    b

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:botara')
-        ->everyMinute();
+        ->everyTenSeconds();
 
         $schedule->call(function () {
             $now = \Carbon\Carbon::now();
