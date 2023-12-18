bf73c1decd663fbcca21387071126892f574ae30
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-18 22:17:38 +0300

    b

--- resources/views/haberfeeds.blade.php
+++ resources/views/haberfeeds.blade.php
@@ -15,7 +15,7 @@
                 <link>{{ 'https://turklog.net/detaylar/' . $haber->id . '/' . Str::slug($haber->baslik) }}</link>
                 <description><![CDATA[{!! $haber->aciklama !!}]]></description>
                 <guid>{{ 'https://turklog.net/detaylar/' . $haber->id . '/' . Str::slug($haber->baslik) }}</guid>
-                <pubDate>{{ {{ date("D, d M Y H:i:s O", strtotime($haber->pubDate)) }} }}</pubDate>
+                <pubDate>{{ date("D, d M Y H:i:s O", strtotime($haber->pubDate)) }}</pubDate>
                 <image>{{ $haber->image }}</image>
             </item>
         @endforeach
