4fc54985aa90613af9445071dc7595cb18bf7369
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-18 22:03:05 +0300

    b

--- resources/views/haberfeeds.blade.php
+++ resources/views/haberfeeds.blade.php
@@ -15,7 +15,12 @@
                 <link>{{ 'https://turklog.net/detaylar/' . $haber->id . '/' . Str::slug($haber->baslik) }}</link>
                 <description><![CDATA[{!! $haber->aciklama !!}]]></description>
                 <guid>{{ 'https://turklog.net/detaylar/' . $haber->id . '/' . Str::slug($haber->baslik) }}</guid>
-                <pubDate>{{ date("D, d M Y G:i:s T", strtotime($haber->pubdate)) }}</pubDate>
+                @php
+                use Carbon\Carbon;
+                // Convert your post's created_at timestamp to a Carbon instance
+                $pubDate = Carbon::parse($haber->created_at)->format('D, d M Y H:i:s') . ' ' . Carbon::parse($haber->created_at)->timezone->getName();
+            @endphp
+                <pubDate>{{ $pubDate }}</pubDate>
                 <image>{{ $haber->image }}</image>
             </item>
         @endforeach
