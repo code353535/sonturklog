844a5a75c81f2172e70b5e03a9b7eb2b3953e4b8
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-26 22:30:04 +0300

    b

--- resources/views/components/front/head.blade.php
+++ resources/views/components/front/head.blade.php
@@ -30,6 +30,28 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7405579499369683"
+     crossorigin="anonymous"></script>
+<!-- turklog -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7405579499369683"
+     data-ad-slot="3800749838"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7405579499369683"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-7405579499369683"
+     data-ad-slot="7471621287"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <meta name="yandex-verification" content="950121826458f0f4" />
         <meta name="google-adsense-account" content="ca-pub-7405579499369683">
