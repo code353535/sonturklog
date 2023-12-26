ec18d6f516ba89ac506e894e76544c415414e63b
code353535 <125445010+code353535@users.noreply.github.com>
2023-12-26 22:40:18 +0300

    b

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -138,7 +138,7 @@
     </div>
   </footer>
   <button id="to-top-button" onclick="goToTop()" title="Go To Top"
-  class="hidden fixed z-50 bottom-[200px] right-10 p-1 border-0 w-8 h-8 shadow-md bg-black hover:bg-orange-500 text-white text-lg font-semibold transition-colors duration-300 max-md:hidden">
+  class="hidden fixed z-50 bottom-80 right-10 p-1 border-0 w-8 h-8 shadow-md bg-black hover:bg-orange-500 text-white text-lg font-semibold transition-colors duration-300 max-md:hidden">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
       <path d="M12 4l8 8h-6v8h-4v-8H4l8-8z" />
   </svg>
